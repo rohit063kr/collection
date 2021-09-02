b8235022e914acc32cd09e1497872948bad3cd90
rohit <rohit063kr@gmail.com>
2021-09-02 04:04:14 -0700

    initial init

---
+++ src/script.js
@@ -0,0 +1,40 @@
+import img1 from './img/1.png';
+import img2 from './img/2.png';
+
+class app {
+  _parentElement = document.querySelector('.container');
+  _data = [
+    {
+      img: `${img1}`,
+      source: 'https://rohit-quiz-game.netlify.app/',
+      title: 'Quiz-game',
+    },
+    {
+      img: `${img2}`,
+      source: 'https://rohit-movie-finder.netlify.app/',
+      title: 'Movie-finder',
+    },
+  ];
+
+  constructor() {
+    this.render(this._data);
+  }
+
+  render(dataArr) {
+    const markup = dataArr
+      .map(el => {
+        return `
+      <a href="${el.source}" class="items">
+        <img src="${el.img}" alt="bg" />
+        <span class="items__detail">${el.title}</span>
+      </a>
+      `;
+      })
+      .join('');
+
+    this._parentElement.innerHTML = '';
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+}
+
+new app();
